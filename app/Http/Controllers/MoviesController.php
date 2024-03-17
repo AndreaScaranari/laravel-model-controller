c1260c2982702944e8943d8bf66f8fe14160681d
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-03-17 17:40:31 +0100

    movies_da_db_e_inviati_a_view

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Movie;
+
 class MoviesController extends Controller
 {
     public function __invoke()
     {
-        return view('movies');
+        $moovies = Movie::all();
+
+        // dd($moovies);
+
+        return view('movies', compact('moovies'));
     }
 }
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -3,6 +3,28 @@
 @section('title', 'Movies')
 
 @section('main-content')
-    <h1>Questa è la pagina MOVIES del sito.</h1>
-    <p>I film li aggiungerò qui</p>
+    <div class="text-center pb-3">
+        <h1>Ecco i film sul nostro database</h1>
+        <h3>Recuperati con Models e inviati alle View con i controller</h3>
+    </div>
+
+    <div class="row row-cols-5 row-gap-3">
+        @foreach ($moovies as $moovie)
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $moovie->title }}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ $moovie->date }}</h6>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet nisi
+                            architecto, quia placeat facere nesciunt adipisci, rerum ullam repellendus similique minima
+                            provident minus reprehenderit blanditiis ipsa officiis saepe, cumque sed.</p>
+                        <div class="d-flex justify-content-between">
+                            <p>{{ $moovie->nationality }}</p>
+                            <p>{{ $moovie->vote }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
